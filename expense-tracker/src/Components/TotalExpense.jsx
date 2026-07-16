d942c67d2172849e5cb7efb6eeb0193f1561e502
Ayush Raj <rajpiyush091@gmail.com>
2026-07-17 01:14:08 +0530

    Added Total expense component

--- expense-tracker/src/Components/TotalExpense.jsx
+++ expense-tracker/src/Components/TotalExpense.jsx
@@ -1,23 +1,23 @@
-function TotalExpense({ expenses}) {
-    const total =  expenses.reduce((acc, expense) => {
-    return acc + expense.amount;
-}, 0);
+function TotalExpense({ expenses }) {
+    const total = expenses.reduce((acc, expense) => {
+        return acc + expense.amount;
+    }, 0);
     return (
-    <div className="max-w-md mx-auto mt-8 bg-purple-600 p-6 rounded-2xl shadow-lg">
+        <div className="max-w-4xl mx-auto mt-8 bg-purple-600 p-8 rounded-2xl shadow-lgflexjustify-betweenitems-center">
 
-        <h2 className="text-xl font-semibold">
-            Total Spending 💰
-        </h2>
+            <h2 className="text-xl font-semibold">
+                Total Spending 💰
+            </h2>
 
-        <h1 className="text-4xl font-bold mt-3">
-            ₹{total}
-        </h1>
+            <h1 className="text-4xl font-bold mt-3">
+                ₹{total}
+            </h1>
 
-        <p className="mt-2 text-purple-200">
-            Your total expenses so far
-        </p>
+            <p className="mt-2 text-purple-200">
+                Your total expenses so far
+            </p>
 
-    </div>
-)
+        </div>
+    )
 }
 export default TotalExpense;
